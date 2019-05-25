4f7c011d362963f64b242e378ec656e235adb0b8
Brian Grant <brian.grant@urbanairship.com>
2019-05-25 14:35:58 -0700

    removed unused packages in url files

--- {{cookiecutter.github_repo}}/local_settings/urls.py
+++ {{cookiecutter.github_repo}}/local_settings/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.views import get_swagger_view
 
 router = DefaultRouter()
--- {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/urls.py
+++ {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.views import get_swagger_view
+
 
 router = DefaultRouter()
 
