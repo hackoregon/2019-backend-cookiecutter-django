73d6d2c0c6e43a3d8b3c18413a852a7f789c6e27
Brian Grant <brian.grant@urbanairship.com>
2019-06-15 13:49:54 -0700

    add notes to urls regarding versioning

--- {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/urls.py
+++ {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/urls.py
@@ -4,6 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
+## We will be using URLPath Versioning:
+
+### add the appropriate versiong keywords to your url paths: ie r'^(?P<version>(v1|v2))/stops/$' for version 1 and 2
 urlpatterns = [
     url(r'^', include(router.urls)),
 ]
